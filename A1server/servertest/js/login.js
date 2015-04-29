16200da1f24bb52aa024ec1ab27d164edef6bf0c
powek <846385116@qq.com>
2015-04-29 14:29:34 +0800

    commit login function test from lujian

---
+++ A1server/servertest/js/login.js
@@ -0,0 +1,21 @@
+function Login(){
+    var userName=$("#uid").val();
+    var userPwd=$("#upwd").val();
+    if(userName==""||userPwd==""){
+        alert("请将信息填写完整！");
+    }else{
+        var url="http://192.168.100.139:8090/WebService1.asmx/Login";
+        appcan.request.getJSON(url,{name:userName,password:userPwd},function(data){
+            if(data.user[0]["status"]=="True"){
+                alert("登录成功！");
+                appcan.window.open({
+                    name:'main',
+                    data:'main.html',
+                    aniId:2,
+                })
+            }else{
+                alert("登陆失败，请核对信息是否正确！");
+            }
+        })
+    }
+}
