91b2bd0a89b85c3ab2f0d455dbfc2f3022e18bb3
memoryfall <527183356@qq.com>
2015-05-08 10:37:31 +0800

    test

--- A1server/servertest/a1server/logqueue.js
+++ A1server/servertest/a1server/logqueue.js
@@ -3,31 +3,50 @@
  * 同步更新本地数据库中的数据
  * 定义其基本控制函数
  */
+function Queue() {
+        this.dataStore = [];
+        this.In_Queue   = In_Queue;
+        this.Out_Queue   = Out_Queue;
+        this.first     = first;
+        this.end       = end;
+        this.toString  = toString;
+        this.Empty_Queue     = Empty_Queue;
+    }
 
-function Init_Queue(q){
-    
-}//初始化提交队列
 
 function In_Queue(q,x){
-    
+    this.dataStore.push(element);
+}//队尾添加一条log
+
+function Out_Queue(){
+    return this.dataStore.shift();
+}//删除队头的log
+
+function Empty_Queue(){
+    if (this.dataStore.length == 0) {
+            return true;
+        } else {
+            return false;
+        }   
 }//
 
-function Out_Queue(q,x){
-    
-}//
-
-function Empty_Queue(q){
-    
-}//
-
-function Front_Queue(q,x){
-    
-}//
-
-
-function ErrorHandleLogqueue(){
-    
+// 可以使用如下方法读取队首和队尾的元素： //
+function first() {
+    return this.dataStore[0];
 }
 
+function end() {
+    return this.dataStore[this.dataStore.length - 1];
+}
+
+// toString()方法显示队列内的所有元素 //
+
+function toString() {
+    var retStr = "";
+    for (var i = 0; i < this.dataStore.length; ++i) {
+        retStr += this.dataStore[i] + "\n";
+    }
+    return retStr;
+}
 
 
