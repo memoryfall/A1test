67231fa66e482bd1bd323ddf98c591fbacf0871f
memoryfall <527183356@qq.com>
2015-05-13 10:39:17 +0800

    delete wrong

---
+++ digiwin/digiwin/Models/ModleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace digiwin.Models
+{
+    public class ModleController : Controller
+    {
+        //
+        // GET: /Modle/
+
+        public class test()
+        {
+           
+        }
+
+    }
+}
