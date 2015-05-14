675642c52d55ea2e16afe0aefd57f069ac35d22e
powek <846385116@qq.com>
2015-05-14 10:57:54 +0800

    lj

--- A1server/servertest/js/login.js
+++ A1server/servertest/js/login.js
@@ -30,6 +30,11 @@ function Login(){
             success:function(data){
                if(data==true){
                    alert("登录成功！");
+                   appcan.window.open({
+                       name:'main',
+                       data:'main.html',
+                       aniId:2,
+                   })
                }else{
                    alert("登录失败！");
                }
@@ -41,20 +46,20 @@ function Login(){
         })
     }
 }
-function initUser(){
-    
-}
-function createDB(){
-    uexDataBaseMgr.openDataBase('userinfo',1);
-    uexDataBaseMgr.cbOpenDataBase=cbOpenDataBase;
-}
-function cbOpenDataBase(opid,type,data){
-    if(data==0){
-        alert("打开数据库成功!");
-    }else{
-        alert("数据库打开失败!");
-    }
-}
+// function initUser(){
+//     
+// }
+// function createDB(){
+    // uexDataBaseMgr.openDataBase('userinfo',1);
+    // uexDataBaseMgr.cbOpenDataBase=cbOpenDataBase;
+// }
+// function cbOpenDataBase(opid,type,data){
+    // if(data==0){
+        // alert("打开数据库成功!");
+    // }else{
+        // alert("数据库打开失败!");
+    // }
+// }
 function insertData(name,password){
     var sql='insert into user values('+"'"+name+"','"+password+"')";
     uexDataBaseMgr.executeSql('userinfo',1,sql);
@@ -67,18 +72,18 @@ function insertDataCallback(opid,type,data){
         alert("插入数据失败！");
     }
 }
-function createTable(){
-    var sql='create table user(name nvarchar(20),password nvarchar(20))';
-    uexDataBaseMgr.executeSql('userinfo',1,sql);
-    uexDataBaseMgr.cbExecuteSql=createTableCallback;
-}
-function createTableCallback(opid,type,data){
-    if(data==0){
-        alert("创建表成功");
-    }else{
-        alert("创建表失败！");
-    }
-}
+// function createTable(){
+    // var sql='create table user(name nvarchar(20),password nvarchar(20))';
+    // uexDataBaseMgr.executeSql('userinfo',1,sql);
+    // uexDataBaseMgr.cbExecuteSql=createTableCallback;
+// }
+// function createTableCallback(opid,type,data){
+    // if(data==0){
+        // alert("创建表成功");
+    // }else{
+        // alert("创建表失败！");
+    // }
+// }
 function transaction(){
     uexDataBaseMgr.transaction('userinfo',1,inFunc)
 }
@@ -94,54 +99,54 @@ function transactionCallback(opid,type,data){
         alert("清空失败");
     }
 }
-function selectData(){
-        var sql = "SELECT * FROM user";
-        uexDataBaseMgr.selectSql('userinfo',1,sql);
-        uexDataBaseMgr.cbSelectSql = selectDataCallback;
-    }
-function selectDataCallback(opid,type,value){
-    if(type==1){
-        var jsonList=eval("("+value+")");
-        if(jsonList.length==0){
-            alert("无数据");
-        }
-        for(var i=0;i<jsonList.length;i++){
-             for(var key in jsonList[i]){
-                 alert(key+":"+jsonList[i][key]);
-                }
-            }
-        
-    }else{
-            alert("查询失败!");
-        }
-}
-function closeDataBase(){
-        uexDataBaseMgr.closeDataBase('userinfo',1);
-        uexDataBaseMgr.cbCloseDataBase = cbCloseDataBase;
-    }
-    function cbCloseDataBase(opid,type,data){
-        if(data == 0){
-            alert("数据库关闭成功！");
-        }else{
-            alert("数据库关闭失败！");
-        }
-    };
-function goToRejist(){
-    appcan.window.open({
-        name:'rejist',
-        data:'rejist.html',
-        aniId:2,
-    })
-}
-function initNewUser(){
-    // var newName=appcan.locStorage.val('NAME');
-    // var newPassword=appcan.locStorage.val('PASSWORD');
-  
-}
-function MissingPwd(){
-    appcan.window.open({
-        name:'password',
-        data:'password.html',
-        aniId:2
-    })
-}
+// function selectData(){
+        // var sql = "SELECT * FROM user";
+        // uexDataBaseMgr.selectSql('userinfo',1,sql);
+        // uexDataBaseMgr.cbSelectSql = selectDataCallback;
+    // }
+// function selectDataCallback(opid,type,value){
+    // if(type==1){
+        // var jsonList=eval("("+value+")");
+        // if(jsonList.length==0){
+            // alert("无数据");
+        // }
+        // for(var i=0;i<jsonList.length;i++){
+             // for(var key in jsonList[i]){
+                 // alert(key+":"+jsonList[i][key]);
+                // }
+            // }
+//         
+    // }else{
+            // alert("查询失败!");
+        // }
+// }
+// function closeDataBase(){
+        // uexDataBaseMgr.closeDataBase('userinfo',1);
+        // uexDataBaseMgr.cbCloseDataBase = cbCloseDataBase;
+    // }
+    // function cbCloseDataBase(opid,type,data){
+        // if(data == 0){
+            // alert("数据库关闭成功！");
+        // }else{
+            // alert("数据库关闭失败！");
+        // }
+    // };
+// function goToRejist(){
+    // appcan.window.open({
+        // name:'rejist',
+        // data:'rejist.html',
+        // aniId:2,
+    // })
+// }
+// function initNewUser(){
+    // // var newName=appcan.locStorage.val('NAME');
+    // // var newPassword=appcan.locStorage.val('PASSWORD');
+//   
+// }
+// function MissingPwd(){
+    // appcan.window.open({
+        // name:'password',
+        // data:'password.html',
+        // aniId:2
+    // })
+// }
