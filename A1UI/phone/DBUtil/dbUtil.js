0f3d2aa413b9e99747fd09cdb91240b8750ea576
powek <846385116@qq.com>
2015-05-22 17:16:46 +0800

    登录和数据库存储

---
+++ A1UI/phone/DBUtil/dbUtil.js
@@ -0,0 +1,80 @@
+
+function createDB(DBname,DBid,cbCreateDB){
+    uexDataBaseMgr.openDataBase(DBname,DBid);
+    uexDataBaseMgr.cbOpenDataBase=cbCreateDB;
+}
+
+
+// function openDataBaseCallback(opid,type,data){
+    // if(data==0){//数据库创建成功
+//         
+    // }else{
+//         
+    // }
+// }
+
+//====================================关闭数据库
+function closeDB(DBname, DBid,cbCloseDataBase){
+     uexDataBaseMgr.closeDataBase(DBname, DBid);
+     uexDataBaseMgr.cbCloseDataBase = cbCloseDataBase;
+}
+
+
+//=======================================创建表
+function createTable(DBname,DBid,sqlExcute,createTableCallback){
+    var sql=sqlExcute;
+    uexDataBaseMgr.executeSql(DBname,DBid,sql);
+    uexDataBaseMgr.cbExecuteSql=createTableCallback;
+}
+
+// function createTableCallback(opid,type,data){
+    // if(data==0){//表创建成功
+//         
+    // }else{
+//         
+    // }
+// }
+
+//=========================================插入数据
+function insertData(DBname,DBid,sqlExcute,insertDataCallback){
+    var sql=sqlExcute;
+    uexDataBaseMgr.executeSql(DBname,DBid,sql);
+    uexDataBaseMgr.cbExecuteSql=insertDataCallback;
+}
+
+// function insertDataCallback(opid,type,data){
+    // if(data==0){//数据插入成功
+//         
+    // }else{
+//         
+    // }
+// }
+
+//==========================================选择数据
+function selectData(DBname,DBid,sqlExcute,selectDataCallback){
+    var sql=sqlExcute;
+    uexDataBaseMgr.selectSql(DBname,DBid,sql);
+    uexDataBaseMgr.cbSelectSql=selectDataCallback;
+}
+// function selectDataCallback(opid,type,value){
+    // if(type==1){//选择操作成功
+//         
+    // }else{
+//         
+    // }
+// }
+
+//===========================================删除数据
+function deleteData(DBname,DBid,sqlExcute,deleteDataCallcak){
+    var sql=sqlExcute;
+    uexDataBaseMgr.selectSql(DBname,DBid,sql);
+    uexDataBaseMgr.cbExecuteSql=deleteDataCallcak;
+}
+
+//事务，增，删  查
+function transaction(DBname,DBid,sqlExcute,excuteCallbacks){
+    var sql=sqlExcute;
+    uexDataBaseMgr.selectSql(DBname,DBid,sql);
+    uexDataBaseMgr.cbExecuteSql=excuteCallbacks;
+}
+
---
+++ A1UI/phone/functionJS/login.js
@@ -0,0 +1,94 @@
+function Login(){
+    var account=$("#LoginAccout").val();
+    var pass=$("#LoginPassword").val();
+    if(account==""||pass==""){
+        alert("输入信息不完整，请重新输入！");
+    }else{
+        appcan.request.ajax({
+            type:'POST',
+            url:'http://180.153.139.167:8080/webapi',
+            data:{
+                Service:'Login',
+                Method:'Read',
+                Account:account,
+                Password:pass
+            },
+            dataType:'json',
+            timeout:300,
+            success:function(data){                
+                if (data.Error == "") {
+                    var DBname = 'userInfo';
+                    var DBid = 1;
+                    var count = 1;
+                    createDB(DBname, DBid, cbCreatDB);
+                    if (count == 1) {
+                        var sqlExcute = 'create table UserInfo(u_Account nvarchar(50) primary key,u_Password nvarchar(50),TenantId int,UserId int,RoleId int)';
+                        createTable(DBname, DBid, sqlExcute, createTableCallback);
+                        count += 1;
+                    }
+                    if (count == 2) {
+                        
+                        var sqlExcute = 'insert into UserInfo values(' + "'" + account + "','" + pass + "'," + data.Data.TenantId + "," + data.Data.UserId + "," + data.Data.RoleId + ")";
+                        insertData(DBname, DBid, sqlExcute, insertDataCallback);
+                        count += 1;
+                    }
+                    if (count==3){
+                        var sqlExcute='select * from userInfo';
+                        selectData(DBname,DBid,sqlExcute,selectDataCallback);
+                    }
+                    localStorage.setItem("TenantId",data.Data.TenantId);
+                    localStorage.setItem("UserId",data.Data.UserId);
+                    localStorage.setItem("RoleId",data.Data.RoleId);
+                }
+
+            },
+            error:function(xhr,type){
+                alert("网络错误！");
+            },
+            offline:true
+        })
+    }
+}
+//=================================创建数据库回调函数
+function cbCreatDB(opid,type,data){
+    if(data==0){
+        alert("数据库打开成功");
+        
+    }else{
+        alert("数据库打开失败");
+    }
+}
+//=================================创建表回调函数
+function createTableCallback(opid,type,data){
+    if(data==0){
+        alert("表创建成功");
+    }else{
+        alert('表创建失败');
+    }
+}
+//================================插入数据回调函数
+function insertDataCallback(opid,type,data){
+    if(data==0){
+        alert("数据插入成功！");
+    }else{
+        alert("数据插入失败！");
+    }
+}
+//===============================筛选数据回调函数
+function selectDataCallback(opid,type,value){
+   
+    if (type == 1) {
+        var jsonList = eval("(" + value + ")");
+        if (jsonList.length == 0) {
+            alert("无数据");
+        }
+        for (var i = 0; i < jsonList.length; i++) {
+            for(var key in jsonList[i]){
+                alert(key+":"+jsonList[i][key]);
+            }
+        }
+    } else {
+        alert("查询失败！");
+    }
+
+}
