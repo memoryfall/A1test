b22a039167998ce37017395db03a81b1154c2082
limo <lizxa@digiwin.biz>
2015-05-26 14:25:12 +0800

    14:24 2015/5/26_A1UI_测试

---
+++ A1UI/phone/mymodel/graphmodel.js
@@ -0,0 +1,14 @@
+
+var graph_data = {
+    data : [getgraphdata(0), 
+            getgraphdata(1), 
+            getgraphdata(2), 
+            getgraphdata(3), 
+            getgraphdata(4), 
+            getgraphdata(5), 
+            getgraphdata(6)
+            ],
+    
+     labels : "incanvasdata"     
+            
+}
